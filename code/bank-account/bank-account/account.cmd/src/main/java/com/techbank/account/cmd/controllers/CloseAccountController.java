a45cd53612e14065b3cc00e6ab3e9ce4a0e50190
azaveri7 <azaveri7@gmail.com>
2025-07-07 15:46:32 +0530

    Functional changes

--- code/bank-account/bank-account/account.cmd/src/main/java/com/techbank/account/cmd/controllers/CloseAccountController.java
+++ code/bank-account/bank-account/account.cmd/src/main/java/com/techbank/account/cmd/controllers/CloseAccountController.java
@@ -6,10 +6,7 @@ import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.MessageFormat;
 import java.util.UUID;
@@ -24,19 +21,17 @@ public class CloseAccountController {
     @Autowired
     private CommandDispatcher commandDispatcher;
 
-    @PostMapping
-    public ResponseEntity<BaseResponse> closeAccount(@RequestBody CloseAccountCommand command) {
-        var id = UUID.randomUUID().toString();
-        command.setId(id);
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> closeAccount(@PathVariable(value = "id") String id) {
         try {
-            commandDispatcher.send(command);
+            commandDispatcher.send(new CloseAccountCommand(id));
             return new ResponseEntity<>(new BaseResponse("Bank Account deletion request completed successfully"),
-                    HttpStatus.CREATED);
+                    HttpStatus.OK);
         } catch (IllegalStateException e) {
             logger.log(Level.WARNING, MessageFormat.format("Client made a bad request - {0}", e.toString()));
             return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            var safeErrorMessage = MessageFormat.format("Error while processing request to open a new bank account for id = {0}", id);
+            var safeErrorMessage = MessageFormat.format("Error while processing request to close a new bank account for id = {0}", id);
             logger.log(Level.SEVERE, safeErrorMessage, e);
             return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
         }
